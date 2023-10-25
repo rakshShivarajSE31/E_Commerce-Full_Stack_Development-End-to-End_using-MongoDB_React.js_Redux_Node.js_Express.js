9376bba82982c73bf7d97ec79c36310a7f040d29
rakshShivarajSE31 <rakshithastudy27@gmail.com>
2023-10-24 21:49:12 -0400

    frontend
    frontend

---
+++ redux/index.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import userSliceReducer from "./userSlice";
+
+import productSlideReducer from "./productSlide";
+
+export const store = configureStore({
+    reducer: {
+       user : userSliceReducer, 
+       product : productSlideReducer
+    },
+});
